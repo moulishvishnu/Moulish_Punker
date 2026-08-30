b050da23aa5acdd5fdf438c3de48b7f431cd8f8f
Moulish Vishnu <moulishvishnu@gmail.com>
2026-08-30 11:11:34 +0530

    Add service worker for PWA asset caching
    Implement service worker for caching PWA assets.
    
    Signed-off-by: Moulish Vishnu <moulishvishnu@gmail.com>

---
+++ Ray's Tips/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'pulse-asia-v1';
+const urlsToCache = [
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+// Install Event: Cache essential PWA assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event: Serve cached assets offline seamlessly
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
